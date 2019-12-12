e8a947544440dd74bb0fdd7706e8ad7403a0dfd0
Preston <pres.10.nap@gmail.com>
2019-12-12 01:51:56 -0500

    other models

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -8,11 +8,27 @@ function findById(id) {
         .first();
 }
 
-function add(user) {
+function add(schemes) {
+    return db('schemes')
+        .insert(scheme)
+        .then(ids => {
+            const [id] = ids;
+
+            return findById(id);
+        })
 }
 
 function update(changes, id) {
+    return db('schemes')
+        .where({ id })
+        .update(changes)
+        .then(post => {
+            return findById(post)
+        })
 }
 
 function remove(id) {
+    return db('schemes')
+        .where({ id })
+        .del();
 }
